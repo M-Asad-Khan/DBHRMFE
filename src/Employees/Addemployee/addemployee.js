a6bf5f74d57463216a756cb559ad545517856eb4
AyshaAreej0 <aysha.areej@devbox.co>
2022-03-14 05:32:41 -0700

    employes

--- src/Employees/Addemployee/addemployee.js
+++ src/Employees/Addemployee/addemployee.js
@@ -3,48 +3,48 @@ import './addemployee.css'
 
 const addemployee = () => {
   return (
-    <div class="container-fluid px-1 py-5 mx-auto">
-    <div class="row d-flex justify-content-center">
+    <div className="container-fluid px-1 py-5 mx-auto">
+    <div className="row d-flex justify-content-center">
        
             
-            <div class="card">
+            <div className="card">
                 
-                <form class="form-card" onsubmit="event.preventDefault()">
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">First name<span class="text-danger"> *</span></label> <input type="text" id="fname" name="fname" placeholder="Enter your first name" onblur="validate(1)"/> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Last name<span class="text-danger"> *</span></label> <input type="text" id="lname" name="lname" placeholder="Enter your last name" onblur="validate(2)"/> </div>
+                <form className="form-card" onsubmit="event.preventDefault()">
+                    <div className="row justify-content-between text-left">
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">First name<span className="text-danger"> *</span></label> <input type="text" id="fname" name="fname" placeholder="Enter your first name" onblur="validate(1)"/> </div>
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Last name<span className="text-danger"> *</span></label> <input type="text" id="lname" name="lname" placeholder="Enter your last name" onblur="validate(2)"/> </div>
                     </div>
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Business email<span class="text-danger"> *</span></label> <input type="text" id="email" name="email" placeholder="" onblur="validate(3)"/> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Phone number<span class="text-danger"> *</span></label> <input type="text" id="mob" name="mob" placeholder="" onblur="validate(4)"/> </div>
+                    <div className="row justify-content-between text-left">
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Business email<span className="text-danger"> *</span></label> <input type="text" id="email" name="email" placeholder="" onblur="validate(3)"/> </div>
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Phone number<span className="text-danger"> *</span></label> <input type="text" id="mob" name="mob" placeholder="" onblur="validate(4)"/> </div>
                     </div>
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Date of Birth<span class="text-danger"> *</span></label> <input type="text" id="DOB" name="DOB" placeholder="" onblur="validate(10)"/> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Joining Date<span class="text-danger"> *</span></label> <input type="text" id="jdate" name="jdate" placeholder="Enter your Joining date" onblur="validate(9)"/> </div>
+                    <div className="row justify-content-between text-left">
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Date of Birth<span className="text-danger"> *</span></label> <input type="text" id="DOB" name="DOB" placeholder="" onblur="validate(10)"/> </div>
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Joining Date<span className="text-danger"> *</span></label> <input type="text" id="jdate" name="jdate" placeholder="Enter your Joining date" onblur="validate(9)"/> </div>
                     </div>
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Designation<span class="text-danger"> *</span></label> <input type="text" id="job" name="job" placeholder="" onblur="validate(5)"/> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Salary<span class="text-danger"> *</span></label> <input type="text" id="job" name="job" placeholder="" onblur="validate(8)"/> </div>
+                    <div className="row justify-content-between text-left">
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Designation<span className="text-danger"> *</span></label> <input type="text" id="job" name="job" placeholder="" onblur="validate(5)"/> </div>
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Salary<span className="text-danger"> *</span></label> <input type="text" id="job" name="job" placeholder="" onblur="validate(8)"/> </div>
                     </div>
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Education<span class="text-danger"> *</span></label> <input type="text" id="ans" name="ans" placeholder="" onblur="validate(6)"/> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Work Experience<span class="text-danger"> *</span></label> <input type="text" id="ans" name="ans" placeholder="" onblur="validate(7)"/> </div>
+                    <div className="row justify-content-between text-left">
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Education<span className="text-danger"> *</span></label> <input type="text" id="ans" name="ans" placeholder="" onblur="validate(6)"/> </div>
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Work Experience<span className="text-danger"> *</span></label> <input type="text" id="ans" name="ans" placeholder="" onblur="validate(7)"/> </div>
                     </div>
                     
-                    <div class="form-group">
-                                            <div class="maxl">
-                                                <label class="radio inline"> 
+                    <div className="form-group">
+                                            <div className="maxl">
+                                                <label className="radio inline"> 
                                                     <input type="radio" name="gender" value="male" checked/>
                                                     <span> Male </span> 
                                                 </label>
-                                                <label class="radio inline"> 
+                                                <label className="radio inline"> 
                                                     <input type="radio" name="gender" value="female"/>
                                                     <span>Female </span> 
                                                 </label>
                                             </div>
                                         </div>
-                    <div class="row justify-content-end">
-                        <div class="form-group col-sm-6"> <button type="submit" class="btn-block btn-primary">Add Employee</button> </div>
+                    <div className="row justify-content-end">
+                        <div className="form-group col-sm-6"> <button type="submit" className="btn-block btn-primary">Add Employee</button> </div>
                     </div>
                 </form>
             </div>
--- src/Teams/Addteams/addteams.js
+++ src/Teams/Addteams/addteams.js
@@ -1,41 +1,62 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './addteams.css'
+import Select from 'react-select';
+
 
 const addteams = () => {
+ const [employee,setEmployee]=useState();
+ const options = [
+    { value: 'andy', label: 'Andy' },
+    { value: 'Aysha', label: 'Aysha' },
+    { value: 'Amna', label: 'Amna' },
+    { value: 'Nancy', label: 'Nancy' },
+    { value: 'El', label: 'Eleven' },
+    { value: 'cadillac', label: 'Cadillac' },
+  ];
   return (
-    <div class="container-fluid px-1 py-5 mx-auto">
-    <div class="row d-flex justify-content-center">
+    <div className="container-fluid px-1 py-5 mx-auto">
+    <div className="row d-flex justify-content-center">
        
             
-            <div class="card">
+            <div className="card">
                 
-                <form class="form-card" onsubmit="event.preventDefault()">
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Team ID<span class="text-danger"> *</span></label> <input type="text" id="tid" name="tid" placeholder="Enter Team ID" onblur="validate(1)"/> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Project Manager<span class="text-danger"> *</span></label> <input type="text" id="pm" name="pm" placeholder="" onblur="validate(2)"/> </div>
+                <form className="form-card" onsubmit="event.preventDefault()">
+                    <div className="row justify-content-between text-left">
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Team ID<span className="text-danger"> *</span></label> <input type="text" id="tid" name="tid" placeholder="Enter Team ID" onblur="validate(1)"/> </div>
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Project Manager<span className="text-danger"> *</span></label> <input type="text" id="pm" name="pm" placeholder="" onblur="validate(2)"/> </div>
                     </div>
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Team Lead<span class="text-danger"> *</span></label> <input type="text" id="tl" name="tl" placeholder="" onblur="validate(10)"/> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Project Name<span class="text-danger"> *</span></label> <input type="text" id="pname" name="pname" placeholder="" onblur="validate(9)"/> </div>
+                    <div className="row justify-content-between text-left">
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Team Lead<span className="text-danger"> *</span></label> <input type="text" id="tl" name="tl" placeholder="" onblur="validate(10)"/> </div>
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Project Name<span className="text-danger"> *</span></label> <input type="text" id="pname" name="pname" placeholder="" onblur="validate(9)"/> </div>
                     </div>
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Enter Start Date<span class="text-danger"> *</span></label> <input type="text" id="date" name="date" placeholder="" onblur="validate(3)"/> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Enter End Date<span class="text-danger"> *</span></label> <input type="text" id="edate" name="edate" placeholder="" onblur="validate(4)"/> </div>
+                    <div className="row justify-content-between text-left">
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Enter Start Date<span className="text-danger"> *</span></label> <input type="text" id="date" name="date" placeholder="" onblur="validate(3)"/> </div>
+                        <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Enter End Date<span className="text-danger"> *</span></label> <input type="text" id="edate" name="edate" placeholder="" onblur="validate(4)"/> </div>
                     </div>
                    
-                    <div class="col-md-6">
-                                        <div class="form-group"> <label for="form_need">Please select Employees *</label> <select id="form_need" name="need" class="form-control"  required="required" data-error="Please specify your need." >
-                                                <option value="" selected disabled>--Select Employees--</option>
-                                                <option>Aysha</option>
-                                                <option>Andy</option>
-                                                <option>Amna</option>
-                                                <option>wajeeha</option>
-                                            </select> </div>
+                    <div className="col-md-6 mb-4">
+                                <div className="form-group"> <label for="form_need">Please select Employees *</label>
+                                <Select
+                                defaultValue={[options[2], options[3]]}
+                                isMulti
+                                name="colors"
+                                options={options}
+                                className="basic-multi-select"
+                                classNamePrefix="select"
+                            />
+                            </div>
+
+                            {/* <select className="selectpicker" multiple searchable="Search here..">
+    <option >USA</option>
+    <option >Germany</option>
+    <option >France</option>
+    <option >Poland</option>
+    <option >Japan</option>
+  </select> */}
                                     </div>
                     
-                    
-                    <div class="row justify-content-end">
-                        <div class="form-group col-sm-6"> <button type="submit" class="btn-block btn-primary">Add Teams</button> </div>
+                    <div className="row justify-content-end">
+                        <div className="form-group col-sm-6"> <button type="submit" className="btn-block btn-primary">Add Teams</button> </div>
                     </div>
                 </form>
             </div>
