2de91b61c2a432c5286053053f193de985c3ebe5
AyshaAreej0 <aysha.areej@devbox.co>
2022-03-14 23:09:32 -0700

    class

--- src/Clients/Addclients/addclients.js
+++ src/Clients/Addclients/addclients.js
@@ -4,32 +4,32 @@ import './addclients.css'
 const addclients = () => {
   return (
 
-   <div class="container-fluid px-1 py-5 mx-auto">
-   <div class="row d-flex justify-content-center">
+   <div className="container-fluid px-1 py-5 mx-auto">
+   <div className="row d-flex justify-content-center">
       
            
-           <div class="card">
+           <div className="card">
                
-               <form class="form-card" onsubmit="event.preventDefault()">
-                   <div class="row justify-content-between text-left">
-                       <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Client ID<span class="text-danger"> *</span></label> <input type="text" id="cid" name="cid" placeholder="" onblur="validate(1)"/> </div>
-                       <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Name<span class="text-danger"> *</span></label> <input type="text" id="lname" name="lname" placeholder="" onblur="validate(2)"/> </div>
+               <form className="form-card" onsubmit="event.preventDefault()">
+                   <div className="row justify-content-between text-left">
+                       <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Client ID<span className="text-danger"> *</span></label> <input type="text" id="cid" name="cid" placeholder="" onblur="validate(1)"/> </div>
+                       <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Name<span className="text-danger"> *</span></label> <input type="text" id="lname" name="lname" placeholder="" onblur="validate(2)"/> </div>
                    </div>
-                   <div class="row justify-content-between text-left">
-                       <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Business email<span class="text-danger"> *</span></label> <input type="text" id="email" name="email" placeholder="" onblur="validate(3)"/> </div>
-                       <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Phone number<span class="text-danger"> *</span></label> <input type="text" id="mob" name="mob" placeholder="" onblur="validate(4)"/> </div>
+                   <div className="row justify-content-between text-left">
+                       <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Business email<span className="text-danger"> *</span></label> <input type="text" id="email" name="email" placeholder="" onblur="validate(3)"/> </div>
+                       <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Phone number<span className="text-danger"> *</span></label> <input type="text" id="mob" name="mob" placeholder="" onblur="validate(4)"/> </div>
                    </div>
-                   <div class="row justify-content-between text-left">
-                       <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Country<span class="text-danger"> *</span></label> <input type="text" id="country" name="country" placeholder="" onblur="validate(10)"/> </div>
-                       <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Project<span class="text-danger"> *</span></label> <input type="text" id="project" name="project" placeholder="" onblur="validate(9)"/> </div>
+                   <div className="row justify-content-between text-left">
+                       <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Country<span className="text-danger"> *</span></label> <input type="text" id="country" name="country" placeholder="" onblur="validate(10)"/> </div>
+                       <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Project<span className="text-danger"> *</span></label> <input type="text" id="project" name="project" placeholder="" onblur="validate(9)"/> </div>
                    </div>
-                   <div class="row justify-content-between text-left">
-                       <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Technology<span class="text-danger"> *</span></label> <input type="text" id="tech" name="tech" placeholder="" onblur="validate(5)"/> </div>
+                   <div className="row justify-content-between text-left">
+                       <div className="form-group col-sm-6 flex-column d-flex"> <label className="form-control-label px-3">Technology<span className="text-danger"> *</span></label> <input type="text" id="tech" name="tech" placeholder="" onblur="validate(5)"/> </div>
                       
                    </div>
                   
-                   <div class="row justify-content-end">
-                       <div class="form-group col-sm-6"> <button type="submit" class="btn-block btn-primary">Add Client</button> </div>
+                   <div className="row justify-content-end">
+                       <div className="form-group col-sm-6"> <button type="submit" className="btn-block btn-primary">Add Client</button> </div>
                    </div>
                </form>
            </div>
