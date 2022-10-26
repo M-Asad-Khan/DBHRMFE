6e598977c9c812e043c172cc4f6a5a7ab617210d
Ali Hasnain <ali.hasnain@adeccogroup.com>
2022-10-26 15:16:30 +0500

    Reset Screen Added

--- src/views/pages/login/Login.js
+++ src/views/pages/login/Login.js
@@ -22,6 +22,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { updateCurrentUserAction } from "src/redux/Login/login.actions";
 import { useHistory } from "react-router-dom";
 import logo from "../../../assets/images/team.jpeg"
+import { userManagmentRequests } from "src/API/UserManagmentApi";
 
 const Login = () => {
   let history = useHistory();
@@ -29,6 +30,7 @@ const Login = () => {
   const dispatch = useDispatch();
   const currentUser = useSelector((state) => state.login.currentUser);
   const [tempLogin, setTempLogin] = useState({});
+  const [isResetPassword, setIsResetPassword] = useState(false);
   const handleChange = (evt) => {
     setTempLogin({
       ...tempLogin,
@@ -61,6 +63,34 @@ const Login = () => {
     }
   };
 
+
+  const resetPassword = async () => {
+    try {
+
+      const res = await loginRequest.loginApi(tempLogin);
+      if (res.error === false) {
+        res.data.user.password = tempLogin.newpassword;
+        res.data.user.user=res.data.user.id;
+        const userUpdate = await userManagmentRequests.updateUser(res.data.user)
+        if (userUpdate.error === false) {
+          alert("Password updated")
+        }
+      }
+      if (res.error === true) {
+
+        if (res?.data?.response.status === 401) {
+          toast.error("Invalid email OR current password !");
+        }
+
+      }
+    } catch (e) {
+      toast.error("error !");
+
+    }
+  };
+
+
+
   return (
     <div className="bg-light min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
@@ -71,65 +101,140 @@ const Login = () => {
               <CCard className="p-4">
 
                 <CCardBody>
-                  <CForm>
-                    <div style={{
-                      textAlign: "center"
-                    }}>
-                      <img
-                        className="rounded-circle mx-auto"
-                        alt="100x100"
-                        style={{ "width": "100px", "height": "100px" }}
-                        src={logo}
-                        data-holder-rendered="true"
-                      />
-                    </div>
+                  {!isResetPassword &&
+                    <CForm>
+                      <div style={{
+                        textAlign: "center"
+                      }}>
+                        <img
+                          className="rounded-circle mx-auto"
+                          alt="100x100"
+                          style={{ "width": "100px", "height": "100px" }}
+                          src={logo}
+                          data-holder-rendered="true"
+                        />
+                      </div>
 
-                    <CInputGroup className="mb-3">
-                      <CInputGroupText>
-                        <CIcon icon={cilUser} />
-                      </CInputGroupText>
-                      <CFormInput
-                        onChange={handleChange}
-                        name="email"
-                        placeholder="Email"
-                      />
-                    </CInputGroup>
-                    <CInputGroup className="mb-4">
-                      <CInputGroupText>
-                        <CIcon icon={cilLockLocked} />
-                      </CInputGroupText>
-                      <CFormInput
-                        onChange={handleChange}
-                        name="password"
-                        type="password"
-                        placeholder="Password"
-                        onKeyPress={(e) => {
-                          if (e.key === "Enter") {
-                            handleLogin()
-                          }
-                        }}
-                      />
-                    </CInputGroup>
-                    <CRow>
-                      <CCol xs={6}>
-                        <CButton
-                          onClick={handleLogin}
-                          color="primary"
-                          className="px-4"
-                        >
-                          Login
-                        </CButton>
-                      </CCol>
-                      {/* <CCol xs={6} className="text-right">
-                        <CButton color="link" className="px-0">
-                          Forgot password?
-                        </CButton>
-                      </CCol> */}
-                    </CRow>
-                  </CForm>
+                      <CInputGroup className="mb-3">
+                        <CInputGroupText>
+                          <CIcon icon={cilUser} />
+                        </CInputGroupText>
+                        <CFormInput
+                          onChange={handleChange}
+                          name="email"
+                          placeholder="Email"
+                        />
+                      </CInputGroup>
+                      <CInputGroup className="mb-4">
+                        <CInputGroupText>
+                          <CIcon icon={cilLockLocked} />
+                        </CInputGroupText>
+                        <CFormInput
+                          onChange={handleChange}
+                          name="password"
+                          type="password"
+                          placeholder="Password"
+                          onKeyPress={(e) => {
+                            if (e.key === "Enter") {
+                              handleLogin()
+                            }
+                          }}
+                        />
+                      </CInputGroup>
+                      <CRow>
+                        <CCol xs={6}>
+                          <CButton
+                            onClick={handleLogin}
+                            color="primary"
+                            className="px-4"
+                          >
+                            Login
+                          </CButton>
+                        </CCol>
+                        <CCol xs={6} className="text-right">
+                          <CButton color="link" className="px-0"
+                            onClick={() => setIsResetPassword(true)}
+                          >
+
+                            Reset password?
+                          </CButton>
+                        </CCol>
+                      </CRow>
+                    </CForm>
+                  }
+                  {isResetPassword &&
+
+                    <CForm>
+                      <div style={{
+                        textAlign: "center"
+                      }}>
+                        <img
+                          className="rounded-circle mx-auto"
+                          alt="100x100"
+                          style={{ "width": "100px", "height": "100px" }}
+                          src={logo}
+                          data-holder-rendered="true"
+                        />
+                      </div>
+
+
+                      <CInputGroup className="mb-3">
+                        <CInputGroupText>
+                          <CIcon icon={cilUser} />
+                        </CInputGroupText>
+                        <CFormInput
+                          onChange={handleChange}
+                          name="email"
+                          placeholder="Email"
+                        />
+                      </CInputGroup>
+
+                      <CInputGroup className="mb-3">
+                        <CInputGroupText>
+                          <CIcon icon={cilUser} />
+                        </CInputGroupText>
+                        <CFormInput
+                          onChange={handleChange}
+                          name="password"
+                          placeholder="Current Password"
+                        />
+                      </CInputGroup>
+
+                      <CInputGroup className="mb-3">
+                        <CInputGroupText>
+                          <CIcon icon={cilUser} />
+                        </CInputGroupText>
+                        <CFormInput
+                          onChange={handleChange}
+                          name="newpassword"
+                          placeholder="New Password"
+                        />
+                      </CInputGroup>
+
+                      <CRow>
+                        <CCol xs={6}>
+                          <CButton
+                            onClick={resetPassword}
+                            color="primary"
+                            className="px-4"
+                          >
+                            Reset Password
+                          </CButton>
+                        </CCol>
+                        <CCol xs={6} className="text-right">
+                          <CButton color="link" className="px-0"
+                            onClick={() => setIsResetPassword(false)}
+                          >
+
+                            Back to Login
+                          </CButton>
+                        </CCol>
+                      </CRow>
+                    </CForm>
+                  }
                 </CCardBody>
               </CCard>
-             
+
             </CCardGroup>
           </CCol>
         </CRow>
